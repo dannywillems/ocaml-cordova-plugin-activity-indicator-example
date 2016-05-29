c574307f92e07eb84c7527ed63012b23a5fee839
Danny Willems <contact@danny-willems.be>
2016-05-29 16:36:49 +0200

    update test

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,6 +1,9 @@
 let on_device_ready _ =
   let t = Cordova_activity_indicator.t () in
-  t#show "Hello world"
+  Cordova.Event.add_event_listener
+    Cordova.Event.Vol_up_button (fun () -> t#show "Hello world") false;
+  Cordova.Event.add_event_listener
+    Cordova.Event.Vol_down_button (fun () -> t#hide ()) false
 
 let _ =
   Cordova.Event.device_ready on_device_ready
